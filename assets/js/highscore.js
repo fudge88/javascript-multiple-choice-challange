fa8a29380dfacc6d98fc67661a582e1e2fe1e299
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-10-13 11:11:02 +0100

    created dynamic html elements in js for highscore- unableto render

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,12 +1,38 @@
-// const scoreStorage = function (data) {
-//   const highScores = JSON.parse(localStorage.getItem(data));
-//   localStorage.setItem("initialScore", JSON.stringify(highScores));
-// };
+const scoreStorage = function (data) {
+  const highScores = JSON.parse(localStorage.getItem(data));
+  localStorage.setItem("initialScore", JSON.stringify(highScores));
+};
 
-// const onLoad = function () {
-//   const highScores = scoreStorage("data", []);
-//   console.log(scoreStorage);
-// };
+const onLoad = function () {
+  const highScoresData = highScores("initialScore", []);
+  console.log(scoreStorage.key);
+};
 
 // window.addEventListener("load", onLoad);
-console.log(localStorage);
+
+const renderScore = function () {
+  const startContainer = document.getElementById("start-container");
+
+  const scoreHeading = document.createElement("h1");
+  scoreHeading.setAttribute("class", "question-font");
+  scoreHeading.textContent = "Scores Table: ";
+
+  const scoreDiv = document.createElement("ul");
+  scoreDiv.setAttribute("class", "answer-btn-container");
+
+  const scoreLoop = function (each, i, array) {
+    const scoreList = document.createElement("li");
+    scoreList.setAttribute("data-message", each);
+    scoreList.setAttribute("class", "myButton");
+    scoreList.textContent = each;
+    startContainer.appendChild(scoreList);
+  };
+
+  localStorage.initialScore.forEach(scoreLoop);
+
+  startContainer.append(scoreHeading, scoreDiv);
+  scoreDiv.appendChild(scoreList);
+  // answerDiv.addEventListener("click", verifyAnswer);
+};
+
+window.addEventListener("load", onLoad);
